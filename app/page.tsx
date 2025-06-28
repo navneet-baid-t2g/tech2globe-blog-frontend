b91e4374bb0132629e02bbee6446b6dee77da468
navneet-baid-t2g <navneet.baid@tech2globe.in>
2025-06-28 18:42:54 +0530

    Homepage server side fetching blogs

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,28 @@
-import Layout from "@/components/layout/Layout"
-import BlogArea1 from "@/components/sections/BlogArea1"
-import BlogCategories1 from "@/components/sections/BlogCategories1"
-import CtaArea1 from "@/components/sections/CtaArea1"
-import HeroArea1 from "@/components/sections/HeroArea1"
-export default function Home() {
+import Layout from "@/components/layout/Layout";
+import BlogArea1 from "@/components/sections/BlogArea1";
+import BlogCategories1 from "@/components/sections/BlogCategories1";
+import CtaArea1 from "@/components/sections/CtaArea1";
+// import HeroArea1 from "@/components/sections/HeroArea1";
 
-	return (
-		<>
+export default async function Home() {
+  const limit = 20;
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_PATH}/posts?page=1&limit=${limit}`,
+    {
+      next: { revalidate: 600 }, 
+    }
+  );
 
-			<Layout headerStyle={1} footerStyle={1}>
-				{/* <HeroArea1 /> */}
-				<BlogArea1 />
-				<BlogCategories1 />
-				<CtaArea1 />
-			</Layout>
-		</>
-	)
+  const json = await res.json();
+  const posts = json?.data?.posts || [];
+  const totalPages = json?.data?.pagination?.totalPages || 1;
+
+  return (
+    <Layout headerStyle={1} footerStyle={1}>
+      {/* <HeroArea1 /> */}
+      <BlogArea1 posts={posts} totalPages={totalPages} currentPage={1} />
+      <BlogCategories1 />
+      <CtaArea1 />
+    </Layout>
+  );
 }
